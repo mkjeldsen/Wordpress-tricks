ecd58400b88756abdb9ce6ea13a6d83cc1fe5e02
Michael Kjeldsen <exp@firewerx.dk>
2014-09-18 14:40:47 +0200

    Create block-spammers-hidden-referer.php

---
+++ block-spammers-hidden-referer.php
@@ -0,0 +1,11 @@
+<?php
+// Add to functions.php
+// Rejects comments made by "users" with an empty HTTP_REFERER - usually direct spam via bots/apps.
+
+function check_referrer() {
+    if (!isset($_SERVER['HTTP_REFERER']) || $_SERVER['HTTP_REFERER'] == “”) {
+        wp_die( __('Please enable referrers in your browser, or, if you're a spammer, bugger off!') );
+    }
+}
+
+add_action('check_comment_flood', 'check_referrer');
